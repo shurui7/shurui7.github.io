baeb7ba66c344a0958d6a33833233b6fc9c972a0
shurui7 <96158419+shurui7@users.noreply.github.com>
2025-09-23 20:19:28 -0400

    1
    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'development' ? '/' : '/shurui7.github.io/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
